883dc6c59c8ffea4f23cf8e272278d5594929f76
unknown <thunder.pedro@hotmail.com>
2023-04-20 14:04:11 -0300

    Pauta

---
+++ src/main/java/com/br/vote/controller/PautaController.java
@@ -0,0 +1,13 @@
+package com.br.vote.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class PautaController {
+
+    @GetMapping("/pauta")
+    public  String index(){
+        return "deded12121312312";
+    }
+
+}
