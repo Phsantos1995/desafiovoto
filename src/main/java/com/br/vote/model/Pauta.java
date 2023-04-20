d5ed328e3944e746166710d952aa5b698e9c4d9d
unknown <thunder.pedro@hotmail.com>
2023-04-20 13:09:06 -0300

    Pauta

---
+++ src/main/java/com/br/vote/model/Pauta.java
@@ -0,0 +1,26 @@
+package com.br.vote.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "PAUTA")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Pauta implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+    @Column(name = "DESCRICAO",nullable = false)
+    private String descricao;
+
+}
